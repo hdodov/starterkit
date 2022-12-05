87bec5fbcd04fa0cd91abe67c3277e0ee4bd9e02
Hristiyan Dodov <h.dodov@gmail.com>
2022-12-05 22:32:44 +0200

    Add url to config.php

--- site/config/config.php
+++ site/config/config.php
@@ -5,6 +5,10 @@ use Kirby\Filesystem\F;
 
 $options = [
     'debug' => true,
+    'url' => [
+        'https://5kz7rdkqszz7wiabx5npv2eeny0maefm.lambda-url.us-east-1.on.aws',
+        'http://starterkit.work'
+    ],
     'panel' => [
         'install' => true
     ],
